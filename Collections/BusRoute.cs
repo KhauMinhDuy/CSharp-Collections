9d6c811c080292afb61949db3016db4aef0a3097
khauminhduy <khauminhduy@gmail.com>
2021-10-15 16:27:37 +0700

    initialize app

---
+++ Collections/BusRoute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    public class BusRoute
+    {
+
+        public int Number { get; }
+        public string Origin { get; }
+        public string Description { get; }
+
+        public BusRoute(int number, string origin, string description)
+        {
+            Number = number;
+            Origin = origin;
+            Description = description;
+        }
+
+        public override string ToString() => $"{Number} : {Origin} -> {Description}";
+    }
+}
