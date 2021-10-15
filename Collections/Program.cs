4909669b9e06ebd550ae8e82a6e467b2588f772a
khauminhduy <khauminhduy@gmail.com>
2021-10-15 18:59:51 +0700

    dictionaries

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -6,8 +6,12 @@ namespace Collections
     {
         static void Main(string[] args)
         {
+            var allRoutes = BusRouteRepository.InitializeRoutes();
 
-            
+            foreach (BusRoute route in allRoutes.Values)
+            {
+                Console.WriteLine(route);
+            }
         }
 
     }
